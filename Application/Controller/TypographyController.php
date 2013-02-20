06c0174abce0c176a9782cac261143330cf11af2
corpsee <poisoncorpsee@gmail.com>
2013-02-20 17:27:29 +0700

    typography

--- Application/Controller/TypographyController.php
+++ Application/Controller/TypographyController.php
@@ -5,7 +5,7 @@ namespace Application\Controller;
 use Framework\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
-class TypoController extends Controller
+class TypographyController extends Controller
 {
 
 	public function index ()
@@ -15,11 +15,11 @@ class TypoController extends Controller
 			'styles'       => array
 			(
 				STYLE_PATH_URL . 'normalize-2.0.1.css',
-				STYLE_PATH_URL . 'newstyle.css',
+				STYLE_PATH_URL . 'typography-0.1.css',
 			),
 			'scripts'      => array
 			(
-				SCRIPT_PATH_URL . 'jquery/jquery-1.8.3.min.js',
+				/*SCRIPT_PATH_URL . 'jquery/jquery-1.8.3.min.js',*/
 			),
 			'page'         => array
 			(
@@ -28,8 +28,7 @@ class TypoController extends Controller
 				'description' => '',
 				'keywords' => ''
 			),
-			'subtemplates' => array('content' => 'typo'),
 		);
-		return $this->render('front_page', $data);
+		return $this->render('typography', $data);
 	}
 }
