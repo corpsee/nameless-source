d2c229987ddda8fd439b36933c229d1d51a63efc
Corpsee <poisoncorpsee@gmail.com>
2013-04-14 01:14:16 +0700

    Localization

---
+++ Nameless/Localization/en/validation.php
@@ -0,0 +1,18 @@
+<?php
+
+// TODO: вынести локализацию в соответствующие модули
+return array
+(
+	'noempty'     => 'The field "%s" can\'t be empty',
+	'number'      => 'The field "%s" isn\'t a number',
+	'decimal'     => 'The field "%s" isn\'t a decimal',
+	'alpha'       => 'The field "%s" may only contain alphabetical characters',
+	'alpha_ext'   => 'The field "%s" isn\'t a extend alphabetic',
+	'email'       => 'The field "%s" isn\'t a valid url',
+	'phone'       => 'The field "%s" isn\'t a valid phone number',
+	'url'         => 'The field "%s" isn\'t a valid url',
+	'length'      => 'The field "%s" must contain %d characters',
+	'min_lehgth'  => 'The field "%s" must contain at least %d characters',
+	'max_length'  => 'The field "%s" can\'t contain more than %d characters',
+	'equal_field' => 'The field "%s" and the field "%s" must be equal',
+);
