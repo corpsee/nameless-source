75eaee57d4595326d8c47d482367f01676a96a8d
Dmitry Khomutov <poisoncorpsee@gmail.com>
2013-05-27 16:02:19 +0700

    Access module improvement

--- Nameless/Modules/Assets/Configs/configuration.php
+++ Nameless/Modules/Assets/Configs/configuration.php
@@ -2,6 +2,6 @@
 
 return array
 (
-	'yuicompressor_path'  => ROOT_PATH . 'Nameless' . DS . 'Modules' . DS . 'Assets' . DS . 'yuicompressor.jar',
-	'java_path'           => '',
+	'yuicompressor_path'  => ROOT_PATH . 'Nameless' . DS . 'Modules' . DS . 'Assets' . DS . 'yuicompressor-2.4.7.jar',
+	'java_path'           => '/usr/bin/java',
 );
