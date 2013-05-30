9e58017731cc068cc0cf4729b6021ad3436cc7a9
Сorpsee <poisoncorpsee@gmail.com>
2013-05-30 16:21:12 +0700

    + LESS support for assets

--- Nameless/Modules/Assets/Configs/configuration.php
+++ Nameless/Modules/Assets/Configs/configuration.php
@@ -7,5 +7,6 @@ return array
 		'yuicompressor_path'  => ROOT_PATH . 'Nameless' . DS . 'Modules' . DS . 'Assets' . DS . 'yuicompressor-2.4.7.jar',
 		'java_path'           => '/usr/bin/java',
 		'path'                => FILE_PATH . 'compiled' . DS,
+		'lessjs_url'          => FILE_PATH_URL . 'scripts/less-1.3.3.min.js'
 	),
 );
