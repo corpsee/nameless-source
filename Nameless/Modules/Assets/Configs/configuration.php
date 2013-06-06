23736b813927b3dc26fcc5260aebd14a9ef2a42f
Сorpsee <poisoncorpsee@gmail.com>
2013-06-06 13:51:00 +0700

    demo page improvement + localization demo

--- Nameless/Modules/Assets/Configs/configuration.php
+++ Nameless/Modules/Assets/Configs/configuration.php
@@ -6,5 +6,5 @@ return array
 	'assets.java_path'           => '/usr/bin/java',
 	'assets.path'                => FILE_PATH . 'compiled' . DS,
 	'assets.less'                => TRUE,
-	'assets.lessjs_url'          => FILE_PATH_URL . 'scripts/less-1.3.3.min.js'
+	'assets.lessjs_url'          => FILE_PATH_URL . 'js/less-1.3.3.min.js'
 );
