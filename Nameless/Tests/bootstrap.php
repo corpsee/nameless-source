d7728cd368ae6fe01db0a66892136b1b7f4bc8f7
Dmitry Khomutov <poisoncorpsee@gmail.com>
2013-07-24 16:21:56 +0700

    Improved: bootstrap for test

---
+++ Nameless/Tests/bootstrap.php
@@ -0,0 +1,16 @@
+<?php
+
+define('DS', DIRECTORY_SEPARATOR);
+
+define('START_TIME',   microtime(TRUE));
+define('START_MEMORY', memory_get_usage());
+
+define('ROOT_PATH',        dirname(dirname(__DIR__)) . DS);
+define('NAMELESS_PATH',    ROOT_PATH . 'Nameless' . DS);
+define('APPLICATION_PATH', ROOT_PATH . 'Application' . DS);
+define('CONFIG_PATH',      APPLICATION_PATH . 'Configs' . DS);
+define('PUBLIC_PATH',      ROOT_PATH . 'Public' . DS);
+define('FILE_PATH',        PUBLIC_PATH . 'files' . DS);
+define('FILE_PATH_URL',    '/files/');
+
+require_once ROOT_PATH . 'Vendors' . DS . 'autoload.php';
