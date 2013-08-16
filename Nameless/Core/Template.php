5df514c88924fc2f63fa7b87660fdc15673cb674
Ñorpsee <poisoncorpsee@gmail.com>
2013-08-16 13:12:23 +0700

    Added: Template filters for data (escape and cleanXSS)

--- Nameless/Core/Template.php
+++ Nameless/Core/Template.php
@@ -14,8 +14,6 @@ namespace Nameless\Core;
 
 use Symfony\Component\HttpFoundation\Response;
 
-//TODO: filters (anti-xss/escape)
-//TODO: subtamplates
 /**
  * Template class
  *
@@ -24,9 +22,19 @@ use Symfony\Component\HttpFoundation\Response;
 class Template
 {
 	/**
-	 * @var Response
+	 * @var string
 	 */
-	protected $response;
+	protected $template_path;
+
+	/**
+	 * @var string
+	 */
+	protected $template_extension;
+
+	/**
+	 * @var string
+	 */
+	protected $template;
 
 	/**
 	 * @var array
@@ -39,57 +47,60 @@ class Template
 	protected static $global_data = array();
 
 	/**
-	 * @var string
+	 * @var boolean
 	 */
-	protected $template_path;
+	protected $template_escape = TRUE;
 
 	/**
-	 * @var string
+	 * @var boolean
 	 */
-	protected $template;
+	protected static $template_global_escape = TRUE;
 
 	/**
-	 * @var string
+	 * @var array
 	 */
-	protected $template_fullpath;
+	protected $escapes = array();
 
 	/**
-	 * @var string
+	 * @var
 	 */
-	protected $template_extension;
+	protected static $global_escapes = array();
 
 	/**
-	 * @param string   $template_path
-	 * @param string   $template_extension
-	 * @param array    $data
-	 * @param string   $template
-	 * @param Response $response
+	 * @var Response
 	 */
-	public function __construct ($template_path, $template_extension = 'tpl', array $data = array(), $template = '', Response $response = NULL)
+	protected $response;
+
+	/**
+	 * @param string $template_path
+	 * @param string $template_extension
+	 */
+	public function __construct ($template_path, $template_extension)
 	{
 		$this->template_path      = $template_path;
 		$this->template_extension = $template_extension;
-		$this->data               = $data;
-		$this->template           = $template;
-		$this->response           = $response;
 	}
 
 	/**
 	 * @param string|array $data_name
 	 * @param mixed        $data_value
+	 * @param bool         $escape
 	 *
 	 * @return Template
+	 *
 	 * @throws \InvalidArgumentException
 	 */
-	public function setData ($data_name, $data_value = NULL)
+	public function setData ($data_name, $data_value = NULL, $escape = TRUE)
 	{
 		if (is_array($data_name) && is_null($data_value))
 		{
-			$this->data = $data_name;
+			$this->data            = $data_name;
+			$this->template_escape = $escape;
 		}
 		elseif ($data_name)
 		{
-			$this->data[$data_name] = $data_value;
+			$this->data[$data_name]    = $data_value;
+			$this->escapes[$data_name] = $escape;
 		}
 		else
 		{
@@ -99,56 +110,88 @@ class Template
 	}
 
 	/**
-	 * @param mixed $data_name
-	 * @param mixed $data_default
-	 *
-	 * @return mixed
-	 * @throws \OutOfBoundsException
-	 */
-	public function getData ($data_name = NULL, $data_default = NULL)
-	{
-		if (is_null($data_name))
-		{
-			return $this->data;
-		}
-		elseif (isset($this->data[$data_name]))
-		{
-			return $this->data[$data_name];
-		}
-		elseif (is_null($data_default))
-		{
-			throw new \OutOfBoundsException('Value doesn`t exist in template data');
-		}
-		return $data_default;
-	}
-
-	/**
-	 * @param mixed $data_name
-	 * @param mixed $data_value
+	 * @param array $escapes
 	 *
 	 * @return Template
 	 */
-	public function bindData ($data_name, &$data_value)
+	protected function setEscapes (array $escapes)
 	{
-		$this->data[$data_name] = $data_value;
+		$this->escapes = $escapes;
+		return $this;
+	}
+
+	/**
+	 * @param string $data_name
+	 *
+	 * @return mixed
+	 *
+	 * @throws \OutOfBoundsException
+	 */
+	public function getData ($data_name = NULL)
+	{
+		if (is_null($data_name))
+		{
+			$data = array();
+			foreach ($this->data as $data_name => $data_value)
+			{
+				if ((isset($this->escapes[$data_name]) && FALSE === $this->escapes[$data_name]) || FALSE === $this->template_escape)
+				{
+					$data[$data_name] = self::cleanXSS($data_value);
+				}
+				else
+				{
+					$data[$data_name] = self::escape($data_value);
+				}
+			}
+			return $data;
+		}
+		elseif (isset($this->data[$data_name]))
+		{
+			if ((isset($this->escapes[$data_name]) && FALSE === $this->escapes[$data_name]) || FALSE === $this->template_escape)
+			{
+				$data_value = self::cleanXSS($this->data[$data_name]);
+			}
+			else
+			{
+				$data_value = self::escape($this->data[$data_name]);
+			}
+			return $data_value;
+		}
+		throw new \OutOfBoundsException('Value doesn`t exist in template data');
+	}
+
+	/**
+	 * @param string $data_name
+	 * @param mixed  $data_value
+	 * @param bool   $escape
+	 *
+	 * @return Template
+	 */
+	public function bindData ($data_name, &$data_value, $escape = TRUE)
+	{
+		$this->data[$data_name]    = $data_value;
+		$this->escapes[$data_name] = $escape;
 		return $this;
 	}
 
 	/**
 	 * @param string|array $data_name
 	 * @param mixed        $data_value
+	 * @param bool         $escape
 	 *
 	 * @throws \InvalidArgumentException
 	 */
-	public static function setGloabalData ($data_name, $data_value = NULL)
+	public static function setGloabalData ($data_name, $data_value = NULL, $escape = TRUE)
 	{
 		if (is_array($data_name) && is_null($data_value))
 		{
-			self::$global_data = $data_name;
+			self::$global_data            = $data_name;
+			self::$template_global_escape = $escape;
 		}
 		elseif ($data_name)
 		{
-			self::$global_data[$data_name] = $data_value;
+			self::$global_data[$data_name]    = $data_value;
+			self::$global_escapes[$data_name] = $escape;
 		}
 		else
 		{
@@ -157,8 +200,8 @@ class Template
 	}
 
 	/**
-	 * @param mixed $data_name
-	 * @param mixed $data_default
+	 * @param string|array $data_name
+	 * @param mixed        $data_default
 	 *
 	 * @return mixed
 	 * @throws \OutOfBoundsException
@@ -167,26 +210,44 @@ class Template
 	{
 		if (is_null($data_name))
 		{
-			return self::$global_data;
+			$data = array();
+			foreach (self::$global_data as $data_name => $data_value)
+			{
+				if ((isset(self::$global_escapes[$data_name]) && FALSE === self::$global_escapes[$data_name]) || FALSE === self::$template_global_escape)
+				{
+					$data[$data_name] = self::cleanXSS($data_value);
+				}
+				else
+				{
+					$data[$data_name] = self::escape($data_value);
+				}
+			}
+			return $data;
 		}
 		elseif (isset(self::$global_data[$data_name]))
 		{
-			return self::$global_data[$data_name];
+			if ((isset(self::$global_escapes[$data_name]) && FALSE === self::$global_escapes[$data_name]) || FALSE === self::$template_global_escape)
+			{
+				$data_value = self::cleanXSS(self::$global_data[$data_name]);
+			}
+			else
+			{
+				$data_value = self::escape(self::$global_data[$data_name]);
+			}
+			return $data_value;
 		}
-		elseif (is_null($data_default))
-		{
-			throw new \OutOfBoundsException('Value doesn`t exist in template data');
-		}
-		return $data_default;
+		throw new \OutOfBoundsException('Value doesn`t exist in template data');
 	}
 
 	/**
-	 * @param mixed $data_name
-	 * @param mixed $data_value
+	 * @param string $data_name
+	 * @param mixed  $data_value
+	 * @param bool   $escape
 	 */
-	public static function bindGlobalData ($data_name, &$data_value)
+	public static function bindGlobalData ($data_name, &$data_value, $escape = TRUE)
 	{
-		self::$global_data[$data_name] = $data_value;
+		self::$global_data[$data_name]    = $data_value;
+		self::$global_escapes[$data_name] = $escape;
 	}
 
 	/**
@@ -265,39 +326,47 @@ class Template
 		return $this->template_extension;
 	}
 
-	public function subTemplate ($subtamplate)
+	/**
+	 * @param string $template
+	 */
+	public function subTemplate ($template)
 	{
-		$subtamplate_instance = new static($this->template_path, $this->template_extension, $this->data, $subtamplate);
+		$subtamplate_instance = new static($this->template_path, $this->template_extension, $this->data, $template);
+		$subtamplate_instance->setTemplate($template);
+		$subtamplate_instance->setData($this->data, $this->template_escape);
+		$subtamplate_instance->setEscapes($this->escapes);
+
 		echo $subtamplate_instance->renderTemplate();
 	}
 
 	/**
 	 * @param string   $template
 	 * @param array    $data
+	 * @param bool     $escape
 	 * @param bool     $compress
 	 * @param Response $response
 	 *
 	 * @return Response
 	 */
-	public function render($template = '', array $data = array(), $compress = FALSE, Response $response = NULL)
+	public function render($template = '', array $data = array(), $escape = TRUE, $compress = FALSE, Response $response = NULL)
 	{
 		if ($data)
 		{
-			$this->data = $data;
+			$this->setData($data, $escape);
 		}
 
 		if ($template)
 		{
-			$this->template = $template;
+			$this->setTemplate($template);
 		}
 
-		if (is_null($response) && is_null($this->response))
+		if ($response)
 		{
-			$this->response = new Response();
+			$this->setResponse($response);
 		}
-		elseif ($response)
+		elseif (is_null($this->response))
 		{
-			$this->response = $response;
+			$this->setResponse(new Response());
 		}
 
 		$content = $this->renderTemplate();
@@ -317,19 +386,19 @@ class Template
 	 */
 	protected function renderTemplate ()
 	{
-		extract($this->data, EXTR_REFS);
+		extract($this->getData(), EXTR_REFS);
 
-		$this->template_fullpath = $this->template_path . $this->template . '.' . $this->template_extension;
-		if (!file_exists($this->template_fullpath))
+		$template_path = $this->template_path . $this->template . '.' . $this->template_extension;
+		if (!file_exists($template_path))
 		{
-			throw new \RuntimeException('Template file: ' . $this->template_fullpath . ' doesn`t exist.');
+			throw new \RuntimeException('Template file: ' . $template_path . ' doesn`t exist.');
 		}
 
 		ob_start();
 
 		try
 		{
-			include_once $this->template_fullpath;
+			include_once $template_path;
 		}
 		catch (\Exception $exception)
 		{
@@ -339,6 +408,11 @@ class Template
 		return ob_get_clean();
 	}
 
+	/**
+	 * @param string $content
+	 *
+	 * @return string
+	 */
 	protected function compressHTML ($content)
 	{
 		$content_parts = preg_split
@@ -391,5 +465,77 @@ class Template
 			$content .= $part;
 		}
 		return $content;
+	}
+
+	/**
+	 * @param $value
+	 *
+	 * @return mixed
+	 * @throws \InvalidArgumentException
+	 */
+	public static function cleanXSS ($value)
+	{
+		$value = (string)$value;
+
+		// Validate standard character entites and UTF16 two byte encoding
+		$value = preg_replace('#(&#*\w+)[\x00-\x20]+;#i', '$1;', $value);
+		$value = preg_replace('#(&#x*)([0-9a-f]+);#i', '$1$2;', $value);
+
+		// Remove carriage returns
+      	$value = preg_replace('#\r+#', '', $value);
+
+		// Remove NULL characters
+		$value = preg_replace('#\0+#', '', $value);
+		$value = preg_replace('#(\\\\0)+#', '', $value);
+
+		$keywords = array
+		(
+			'#\bj\s*a\s*v\s*a\s*s\s*c\s*r\s*i\s*p\s*t\b#is', // javascript
+			'#\bv\s*b\s*s\s*c\s*r\s*i\s*p\s*t\b#is', // vbscript
+			'#\bv\s*b\s*s\s*c\s*r\s*p\s*t\b#is', // vbscrpt
+			'#\bs\s*c\s*r\s*i\s*p\s*t\b#is', //script
+			'#\ba\s*p\s*p\s*l\s*e\s*t\b#is', // applet
+			'#\ba\s*l\s*e\s*r\s*t\b#is', // alert
+			'#\bd\s*o\s*c\s*u\s*m\s*e\s*n\s*t\b#is', // document
+			'#\bw\s*r\s*i\s*t\s*e\b#is', // write
+			'#\bc\s*o\s*o\s*k\s*i\s*e\b#is', // cookie
+			'#\bw\s*i\s*n\s*d\s*o\s*w\b#is' // window
+		);
+
+		// Compact exploded keywords like "j a v a s c r i p t"
+		foreach ($keywords as $keyword)
+		{
+			$marches = array();
+			preg_match_all($keyword, $value, $marches);
+
+			foreach ($marches[0] as $match)
+			{
+				$value = str_replace($match, preg_replace('/\s*/', '', $match), $value);
+			}
+		}
+
+		$keywords = array
+		(
+			'/<(a|img)[^>]*[^a-z](<script|<xss)[^>]*>/is',
+			'/<(a|img)[^>]*[^a-z]document\.cookie[^>]*>/is',
+			'/<(a|img)[^>]*[^a-z]vbscri?pt\s*:[^>]*>/is',
+			'/<(a|img)[^>]*[^a-z]expression\s*\([^>]*>/is',
+			'#vbscri?pt\s*:#is',
+			'#javascript\s*:#is',
+			'#<\s*embed.*swf#is',
+			'#<(a|img)[^>]*[^a-z]alert\s*\([^>]*>#is',
+			'#<(a|img)[^>]*[^a-z]javascript\s*:[^>]*>#is',
+			'#<(a|img)[^>]*[^a-z]window\.[^>]*>#is',
+			'#<(a|img)[^>]*[^a-z]document\.[^>]*>#is',
+			'#<[^>]*[^a-z]on[a-z]*\s*=[^>]*>#is',
+		);
+
+		return preg_replace($keywords, '', $value);
+	}
+
+	public static function escape ($value)
+	{
+		$value = (string)$value;
+		return htmlspecialchars($value, ENT_NOQUOTES | ENT_SUBSTITUTE, 'UTF-8');
 	}
 }
