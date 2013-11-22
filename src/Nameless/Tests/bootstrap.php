7549b83f73551abb7074ab8deed0ff7e90d71b00
Dmitry Khomutov <poisoncorpsee@gmail.com>
2013-11-22 16:55:37 +0700

    Removed constant NAMELESS_PATH into the framework

--- src/Nameless/Tests/bootstrap.php
+++ src/Nameless/Tests/bootstrap.php
@@ -6,8 +6,6 @@ define('START_TIME',   microtime(TRUE));
 define('START_MEMORY', memory_get_usage());
 
 define('ROOT_PATH',        dirname(dirname(__DIR__)) . DS);
-//TODO: remove this const
-define('NAMELESS_PATH',    ROOT_PATH . 'Nameless' . DS);
 define('APPLICATION_PATH', ROOT_PATH . 'Application' . DS);
 define('CONFIG_PATH',      APPLICATION_PATH . 'configs' . DS);
 define('PUBLIC_PATH',      ROOT_PATH . 'Public' . DS);
