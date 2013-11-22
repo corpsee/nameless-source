a038d5d7d37caecef71b95dfedd8b4c39097d982
Corpsee <poisoncorpsee@gmail.com>
2013-11-22 14:54:32 +0700

    Update composer.json for packegist.com install

--- Application/localization/en/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return array
-(
-	'h2'  => 'Framework',
-	'p'   => 'In computer programming, a software framework is an abstraction in which software providing generic functionality can be selectively changed by additional user-written code, thus providing application-specific software. A software framework is a universal, reusable software platform used to develop applications, products and solutions.',
-	'btn' => 'View details »',
-);
