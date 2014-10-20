1ec0b9d8da683b16dec4631c3442f14ff6afa5e4
Dmitry Khomutov <poisoncorpsee@gmail.com>
2014-10-20 23:44:39 +0700

    Fixed error pages (for Bootstrap v3)

--- src/Core/ErrorController.php
+++ src/Core/ErrorController.php
@@ -6,11 +6,10 @@ use Symfony\Component\Debug\Exception\FlattenException;
 
 class ErrorController extends Controller
 {
-    //TODO: error style - bootstrap
     public function error(FlattenException $exception)
     {
         $data = [
-            'styles' => ['/files/lib/bootstrap/2.3.2/css/bootstrap.css'],
+            'styles' => ['/files/lib/bootstrap/3.2.0/css/bootstrap.css'],
         ];
 
         return $this->render($exception->getStatusCode(), $data, Template::FILTER_ESCAPE, [], null, NAMELESS_PATH . 'Core' . DS . 'templates' . DS);
